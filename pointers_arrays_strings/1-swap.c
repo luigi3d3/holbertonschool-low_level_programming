f9c0c8b8c44a46c20befb6d5dca7046377c80ae3
root <luigino.loayza@gmail.com>
2023-10-27 08:59:07 -0700

    corrigiendo estilo betty 2

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 /**
  * swap_int - intercambia valores a travez de punteros
- * @a: hace intercambio por puntero con el aux 
+ * @a: hace intercambio por puntero con el aux
  * @b: hace intercambio por puntero
  * Description: el valor de la primera variable es guardada en el auxiliar
- * Return: 0 
+ * Return: 0
  */
 
 void swap_int(int *a, int *b)
