59c3a7ff14395bedaa45370f072fe89a4018ed5f
root <luigino.loayza@gmail.com>
2023-10-27 11:00:05 -0700

    agregue betti, main.h, 2-main.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,17 @@
+/**
+ * _strlen - medir devuelve la longitud de una cadena
+ * @s: es la variable con la que va recorre la cadena
+ * Descrition: con el bucle while, recorre la cadena hasta encontrar el terminal null
+ * Return: length
+ */
+
 #include "main.h"
 
 int _strlen(char *s){
 	int length = 0;
 
 	while(*s != '\0'){
-		length++
+		length++;
 	}
-	return length
+	return length;
 }
