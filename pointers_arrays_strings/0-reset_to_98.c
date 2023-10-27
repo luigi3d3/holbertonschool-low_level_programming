f411a3589f273a284c311d7fb1dd81d2ec2ac474
root <luigino.loayza@gmail.com>
2023-10-26 19:01:27 -0700

    agrego documentacion betty

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,11 @@
+
+/**
+ * reset_to_98 - cambia el valor del parametro que recive del puntero
+ * @n: parametro puntero de tipo int
+ * Return: nada
+ */ 
+
+
 void reset_to_98(int *n)
 {
 	*n = 98;	
