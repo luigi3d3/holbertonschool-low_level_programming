d66ffa960c944c9a2d92ef07e132fd7892e1c069
root <luigino.loayza@gmail.com>
2023-10-27 09:37:26 -0700

    correcion del ejercicio

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -7,3 +7,10 @@ int _strlen(char *s){
 	}
 
 }
+int main(void) {
+	    char str[] = "Hello, World!";
+	        int length = _strlen(str);
+		    printf("Length of the string: %d\n", length);
+
+		        return 0;
+}
