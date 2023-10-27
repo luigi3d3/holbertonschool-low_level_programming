00bee90747f549eb161cd8902cc53524510d4782
root <luigino.loayza@gmail.com>
2023-10-26 19:05:35 -0700

    borra espacios innecesarios

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 
 /**
  * reset_to_98 - cambia el valor del parametro que recive del puntero
- * @n: parametro puntero de tipo int
+ * @n:o parametro puntero de tipo int
  * Return: nada
- */ 
+ */
 
 
 void reset_to_98(int *n)
 {
-	*n = 98;	
+	*n = 98;
 }
