f8fa32fd112ff1ef3c62cfbef4d47788955c9ddc
root <luigino.loayza@gmail.com>
2023-10-29 10:31:52 -0700

    modificacion de estilo betty

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 /**
  * print_rev -Escribe una función que imprima una cadena, al revés.
  * @s: es la variable con la que va a recorre la cadena
- * Descrition: imprime una cadena al revez 
+ * Descrition: imprime una cadena al reves
  * Return: cadena inversa
  */
 
@@ -23,5 +23,5 @@ void print_rev(char *s)
 		putchar(s[i]);
 	}
 
-	putchar('\n');	
+	putchar('\n');
 }
