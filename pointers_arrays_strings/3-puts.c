7649eea436cf070afe9ad378ccfcaf29e771e891
root <luigino.loayza@gmail.com>
2023-10-28 17:30:59 -0700

    I do not fear computers. I fear the lack of them

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+/**
+ * _puts - Escribe una función que imprima una cadena,
+ *  seguida de una nueva línea
+ *  @str: toma un puntero a una matriz de caracteres como argumento.
+ *  Description: un string lo convierte en texto impreso
+ *  Return: 0
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+void _puts(char *str)
+{
+	while( *str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
