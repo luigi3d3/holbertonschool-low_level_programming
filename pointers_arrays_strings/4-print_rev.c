e5907cea7b69cd07179f88c9aa6ab2ee22c91471
root <luigino.loayza@gmail.com>
2023-10-29 10:28:22 -0700

    I can only go one way. I've not got a reverse gear

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+/**
+ * print_rev -Escribe una función que imprima una cadena, al revés.
+ * @s: es la variable con la que va a recorre la cadena
+ * Descrition: imprime una cadena al revez 
+ * Return: cadena inversa
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+
+	putchar('\n');	
+}
