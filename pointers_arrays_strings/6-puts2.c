cec5606eed7103c591b2a29f6be65001b3d50aef
root <luigino.loayza@gmail.com>
2023-10-29 16:58:08 -0700

    prototipo que imprime una cadena cada dos valores

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+/**
+ * puts2 - Escibre una funcion que impre una 
+ * cadena cada dos valores
+ * @str: toma un puntero a una matriz de caracteres
+ * como argumento.
+ * Descrition: imprime una cadena cada dos valores
+ * Return: 0
+ */
+
+
+#include "main.h"
+#include <stdio.h>
+
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if( i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+		i++;
+	}
+	putchar('\n');
+}
