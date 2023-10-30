297a7f52aa572e6f7dbd96adb487642ec3663b3b
root <luigino.loayza@gmail.com>
2023-10-29 20:53:33 -0700

    correcion del prototipo agregandoy corrigiendo betty

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i ++)
+	for (i = 0; i < n; i++)
 	{
 		if (i > 0)
 		{
-			printf(",");
+			printf(", ");
 		}
 		printf("%d", a[i]);
 	}
