147d81b4a0d3f946e0c38ee7869ce6de2771caf0
root <luigino.loayza@gmail.com>
2023-10-29 20:48:34 -0700

    funcion que convierte un array en cadena

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+/**
+ * print_array - Escribe una cadena de apartir de un array
+ * @a : toma un puntero a una matriz de caracteres como argumento.
+ * @n : es la cantidad de elementos que hay dentro de un array
+ * Descrition: imprime un array en una cadena
+ * Return: 0
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i ++)
+	{
+		if (i > 0)
+		{
+			printf(",");
+		}
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
