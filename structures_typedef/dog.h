1792b32267368201817cc0903a923b79106afaef
root <luigino.loayza@gmail.com>
2023-11-03 09:40:03 -0700

    realizacion del struc poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct god - Realizar definiciones de tipo
+ * Description: Definicion de los tipos de struct
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
