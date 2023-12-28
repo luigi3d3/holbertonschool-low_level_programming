95ac89f1ee729d3f240b8d726ee02e0cd079eaaa
root <luigino.loayza@gmail.com>
2023-12-28 08:23:13 -0800

    ejercico3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - int
+ * @c: the character checked
+ * Description: checks for lowercase character
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
