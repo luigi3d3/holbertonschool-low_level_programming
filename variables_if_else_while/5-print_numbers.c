5b2812fae6dd47aec556c7307145a25dd5994455
root <luigino.loayza@gmail.com>
2023-12-28 07:30:20 -0800

    ejercicio 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - tasks
+ * Description: task
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
