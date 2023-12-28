300c49aa284ca631b8bb4db975109cfd1f42b7a1
root <luigino.loayza@gmail.com>
2023-12-28 07:26:28 -0800

    ejercicio 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - tasks
+ * Description: task
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar (letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+return (0);
+}
