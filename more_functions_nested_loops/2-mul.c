a1449b4fc2b280f02307fc1fbaac7b1d2a64d739
root <luigino.loayza@gmail.com>
2023-12-28 13:31:02 -0800

    ejercicio2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - int
+ * @a: the character checked
+ * @b: the character checked
+ * Description: checks for a digit(0 through 9)
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int mul(int a, int b)
+	{
+	return (a * b);
+	}
