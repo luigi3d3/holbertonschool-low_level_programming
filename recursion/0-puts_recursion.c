070283046591f5ae765b7e26729c0a68fd3f6af5
root <luigino.loayza@gmail.com>
2023-12-28 14:15:17 -0800

    correcion estilo ejercicio0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line.
- * @s: The string to be printed.
+ * _puts_recursion - char
+ * @s: checked
+ * Description: Write a function that prints a string
+ * Return: 0
  */
+
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
+	if (*s == '\0')
+		{
+		_putchar('\n');
+		return;
+		}
 
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
