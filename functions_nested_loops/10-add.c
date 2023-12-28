e6bd25a6ad9d20fd1212b4f0c1c0d414e96abaf2
root <luigino.loayza@gmail.com>
2023-12-28 11:09:33 -0800

    ejercicio 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - int
+ * @a @b: the character checked
+ * Description: adds two integers
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
