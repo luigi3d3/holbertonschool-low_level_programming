e3235c6f6fa6dee441a2fd56a1bf6190e8b31917
root <luigino.loayza@gmail.com>
2023-12-28 13:25:04 -0800

    ejercicio 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - int
+ * @c: the character checked
+ * Description: checks for uppercase character
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
