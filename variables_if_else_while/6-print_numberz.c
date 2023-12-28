16007ccef4d94ad16c18fb4ee4b6bde7be2afc64
root <luigino.loayza@gmail.com>
2023-12-28 07:31:29 -0800

    ejercicio 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - tasks
+ * Description: task
+ * Return: 0
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		putchar('0' + y);
+	}
+	putchar('\n');
+
+return (0);
+}
