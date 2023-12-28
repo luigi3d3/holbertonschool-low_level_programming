cab83cebd8229411140c52ef01b8923ea26e09a5
root <luigino.loayza@gmail.com>
2023-12-28 09:05:51 -0800

    ejercicio8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - void
+ * print_digit - print
+ * @digit: checked
+ * Description: prints every minute of the day of Jack Bauer
+ * Return: 0
+ */
+
+void print_digit(int digit)
+{
+	_putchar(digit + '0');
+}
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+			{
+			print_digit(hour / 10);
+
+			print_digit(hour % 10);
+			_putchar(':');
+
+			print_digit(minute / 10);
+
+			print_digit(minute % 10);
+			_putchar('\n');
+			}
+	}
+}
