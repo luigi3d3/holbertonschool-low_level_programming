0924937751b5250766bd3db00c82db319ece074e
root <luigino.loayza@gmail.com>
2023-12-28 21:02:05 -0800

    ejercicio5

--- recursion/main.h
+++ recursion/main.h
@@ -9,7 +9,8 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-
+int _sqrt_getter(int n, int guess);
+int is_prime_helper(int n, int i);
 
 
 #endif
