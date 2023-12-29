ba8e83f4ce1657c083f99221e4cef477bf11ea35
root <luigino.loayza@gmail.com>
2023-12-28 16:48:41 -0800

    correcion del ejercicio4

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,6 +7,7 @@
  * @owner : definicion del dueño
  * @age : definicion de la edad
  * Description: Definicion de los tipos de struct
+ * Return:
  */
 
 typedef struct dog
@@ -18,5 +19,8 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
